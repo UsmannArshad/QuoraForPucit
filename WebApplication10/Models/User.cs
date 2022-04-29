b64ae90d42c37af96707ffb5f62349b10d8fffdb
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-04-29 21:26:01 +0500

    Sign up Page made and displayed

---
+++ WebApplication10/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication10.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public int Password { get; set; }
+        public string name { get; set; }
+    }
+}
